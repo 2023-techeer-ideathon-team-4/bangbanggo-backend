30b12d82968e4490b4762f3d7a7517a3e5e3dd86
sunwoonana <sunwoonana@gmail.com>
2023-05-27 03:55:34 +0900

    #4/장고-model-수정

--- sight/models.py
+++ sight/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 
 class Place(models.Model):
-    place = models.CharField(max_length=50)
+    place = models.CharField(max_length=50, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class Input(models.Model):
     addr = models.CharField(max_length=50)
     kilo = models.FloatField()
-    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    place_id = models.ForeignKey(Place, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
